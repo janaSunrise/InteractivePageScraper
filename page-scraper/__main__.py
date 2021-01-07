4a77df74da34f901bdebfbdcb2ba9ef75d2ebb3d
janaSunrise <warriordefenderz@gmail.com>
2021-01-07 11:03:53 +0530

    Refactor the code

--- page-scraper/__main__.py
+++ page-scraper/__main__.py
@@ -3,12 +3,13 @@ from .utils import get_website_content
 from bs4 import BeautifulSoup
 import inquirer
 
+choices = ["Headings and Subheadings", "Links", "Images", "Tables"]
 questions = [
     inquirer.Text("site", message="Which site do you want to scrape"),
     inquirer.List(
         "parse_object",
         message="Which object do you want to parse",
-        choices=["Headings and Subheadings", "Links", "Images", "Tables"]
+        choices=choices
     )
 ]
 answers = inquirer.prompt(questions)
@@ -24,22 +25,22 @@ lks = soup.find_all(["h1", "h2", "h3"])
 
 print(lks[0].text)
 
-choice = answers["parse_object"].lower()
-if choice == "headings and subheadings":
+choice = answers["parse_object"]
+if choice == choices[0]:
     headings = soup.find_all(["h1", "h2", "h3"])
 
     if not headings or len(headings) == 0:
         print("No headings found!")
     else:
         headings = [heading.text for heading in headings]
-elif choice == "links":
+elif choice == choices[1]:
     links = soup.find_all("a")
 
     if not links or len(links) == 0:
         print("No links found!")
     else:
         links = [(link["href"], link.text) for link in soup.find_all("a")]
-elif choice == "images":
+elif choice == choices[2]:
     images = soup.find_all("img")
 
     if not images or len(images) == 0:
